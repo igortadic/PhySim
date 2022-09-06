be11b996afc7fdb5fac6cdb1912bf10fc6978bab
TehStud <igortadic.msc@gmail.com>
2022-09-06 10:52:07 +0200

    creating the main apple object

--- physim.py
+++ physim.py
@@ -1,8 +1,16 @@
 import pygame, sys, pymunk
 
+
+def create_apple(space):
+	body = pymunk.Body(1, 100, body_type = pymunk.Body.DYNAMIC)
+	body.position = (400, 0)
+	shape = pymunk.Circle(body, 80)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 800)) #creating the display surface
 clock = pygame.time.Clock() #creating the clock
+space = pymunk.Space()
+space.gravity = (0,500)
 
 
 while True: #infinite loop
@@ -13,5 +21,6 @@ while True: #infinite loop
 
 
 	screen.fill((217, 217, 217)) #background colour
+	space.step(1/50)
 	pygame.display.update() #frame rendering
 	clock.tick(120) #120 frames per second
