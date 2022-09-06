ac4d03369f0e20ab73548966ea4d5a56e4959660
TehStud <igortadic.msc@gmail.com>
2022-09-06 23:15:35 +0200

    adding a static object ball

--- physim.py
+++ physim.py
@@ -5,12 +5,42 @@ def create_apple(space):
 	body = pymunk.Body(1, 100, body_type = pymunk.Body.DYNAMIC)
 	body.position = (400, 0)
 	shape = pymunk.Circle(body, 80)
+	space.add(body, shape)
+	return shape
+
+def draw_apples(apples):
+	for apple in apples:
+		pos_x = int(apple.body.position.x)
+		pos_y = int(apple.body.position.y)
+		pygame.draw.circle(screen,(0,0,0),(pos_x,pos_y),80)
+
+
+def static_ball(space):
+	body = pymunk.Body(body_type = pymunk.Body.STATIC)
+	body.position = (500, 500)
+	shape = pymunk.Circle(body, 50)
+	space.add(body, shape)
+	return shape
+
+
+def draw_static_ball(balls):
+	for ball in balls:
+		pos_x = int(ball.body.position.x)
+		pos_y = int(ball.body.position.y)
+		pygame.draw.circle(screen,(0,0,0),(pos_x,pos_y),80)
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800)) #creating the display surface
 clock = pygame.time.Clock() #creating the clock
 space = pymunk.Space()
-space.gravity = (0,500)
+space.gravity = (0,200)
+apples = []
+apples.append(create_apple(space)) 
+
+
+balls = []
+balls.append(static_ball(space))
 
 
 while True: #infinite loop
@@ -21,6 +51,8 @@ while True: #infinite loop
 
 
 	screen.fill((217, 217, 217)) #background colour
+	draw_apples(apples)
+	draw_static_ball(balls)
 	space.step(1/50)
 	pygame.display.update() #frame rendering
 	clock.tick(120) #120 frames per second
